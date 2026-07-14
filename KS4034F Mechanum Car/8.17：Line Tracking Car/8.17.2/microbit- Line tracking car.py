b1c46fe06bb9b78a998d46f6779972c4f31b2dfd
Mike <pfeiffer3000@gmail.com>
2026-07-14 10:40:36 -0400

    Update microbit- Line tracking car.py

--- KS4034F Mechanum Car/8.17：Line Tracking Car/8.17.2/microbit- Line tracking car.py
+++ KS4034F Mechanum Car/8.17：Line Tracking Car/8.17.2/microbit- Line tracking car.py
@@ -7,21 +7,23 @@ val_L = 0
 val_C = 0
 val_R = 0
 
+speed = 50
+
 while True:
     val_L = pin3.read_digital()
     val_C = pin4.read_digital()
     val_R = pin10.read_digital()
     if val_C == 0:
         if val_L == 0 and val_R == 1:
-            mecanumCar.Motor_Upper_L(1, 80)
-            mecanumCar.Motor_Lower_L(1, 80)
-            mecanumCar.Motor_Upper_R(0, 80)
-            mecanumCar.Motor_Lower_R(0, 80)
+            mecanumCar.Motor_Upper_L(1, speed)
+            mecanumCar.Motor_Lower_L(1, speed)
+            mecanumCar.Motor_Upper_R(0, speed)
+            mecanumCar.Motor_Lower_R(0, speed)
         elif val_L == 1 and val_R == 0:
-            mecanumCar.Motor_Upper_L(0, 80)
-            mecanumCar.Motor_Lower_L(0, 80)
-            mecanumCar.Motor_Upper_R(1, 80)
-            mecanumCar.Motor_Lower_R(1, 80)
+            mecanumCar.Motor_Upper_L(0, speed)
+            mecanumCar.Motor_Lower_L(0, speed)
+            mecanumCar.Motor_Upper_R(1, speed)
+            mecanumCar.Motor_Lower_R(1, speed)
         else:
             mecanumCar.Motor_Upper_L(0, 0)
             mecanumCar.Motor_Lower_L(0, 0)
@@ -29,18 +31,18 @@ while True:
             mecanumCar.Motor_Lower_R(0, 0)
     else :
         if val_L == 0 and val_R == 1:
-            mecanumCar.Motor_Upper_L(1, 80)
-            mecanumCar.Motor_Lower_L(1, 80)
-            mecanumCar.Motor_Upper_R(0, 80)
-            mecanumCar.Motor_Lower_R(0, 80)
+            mecanumCar.Motor_Upper_L(1, speed)
+            mecanumCar.Motor_Lower_L(1, speed)
+            mecanumCar.Motor_Upper_R(0, speed)
+            mecanumCar.Motor_Lower_R(0, speed)
         elif val_L == 1 and val_R == 0:
-            mecanumCar.Motor_Upper_L(0, 80)
-            mecanumCar.Motor_Lower_L(0, 80)
-            mecanumCar.Motor_Upper_R(1, 80)
-            mecanumCar.Motor_Lower_R(1, 80)
+            mecanumCar.Motor_Upper_L(0, speed)
+            mecanumCar.Motor_Lower_L(0, speed)
+            mecanumCar.Motor_Upper_R(1, speed)
+            mecanumCar.Motor_Lower_R(1, speed)
         else:
-            mecanumCar.Motor_Upper_L(1, 80)
-            mecanumCar.Motor_Lower_L(1, 80)
-            mecanumCar.Motor_Upper_R(1, 80)
-            mecanumCar.Motor_Lower_R(1, 80)
+            mecanumCar.Motor_Upper_L(1, speed)
+            mecanumCar.Motor_Lower_L(1, speed)
+            mecanumCar.Motor_Upper_R(1, speed)
+            mecanumCar.Motor_Lower_R(1, speed)
 
